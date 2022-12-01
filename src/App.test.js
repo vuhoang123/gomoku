941a0d7e025f6985b23da44c578879779dfb7952
hatieudao <froyal2kk1@gmail.com>
2022-12-01 13:05:50 +0700

    fix: test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "../App";
+import App from "./src/App";
 import renderer from "react-test-renderer";
 
 test("renders learn react link", () => {
